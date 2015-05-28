b28c89ab647524351c942d0d284573b9347204ee
unknown <ryan.cullen@live.ca>
2015-05-28 15:43:23 -0400

    added exp table

--- dnd/Character.java
+++ dnd/Character.java
@@ -7,6 +7,7 @@ public class Character
 	private int level, maxHP, currentHP, ac, fort, ref, will, speed, exp, gold, initiative, maxSurges, currentSurges;
 	private String name, notes;
 	private ArrayList<Item> items = new ArrayList<String>();
+	private static final int[] EXP_TABLE = [-1, 0, 1000, 2250, 3750, 5500, 7500, 10000, 13000, 16500, 20500, 26000, 32000, 39000, 47000, 57000, 69000, 83000, 99000, 119000, 143000, 175000, 210000, 255000, 310000, 375000, 450000, 550000, 675000, 825000, 1000000];
 	
 	public Character(String name, int level, int exp; int hp, int surges, int ac, int fort, int ref, int will, int speed, int gold)
 	{
